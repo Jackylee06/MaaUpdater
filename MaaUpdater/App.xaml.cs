fc4209915a54d03798ae7ad5819c085218f5b68f
Jackylee06 <jacky_lee666@163.com>
2024-08-14 21:12:50 +0800

    添加项目文件。

---
+++ MaaUpdater/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MaaUpdater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MaaUpdater/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace MaaUpdater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (!File.Exists(".\\MAA.exe"))
+            {
+                if (MessageBox.Show("请将MAA更新器移动到MAA根目录下后重新启动", "错误", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK) == MessageBoxResult.OK)
+                {
+                    this.Close();
+                }
+            }
+            if (!File.Exists(".\\MaaUpdater.json"))
+            {
+                LogInfo.Text += "请在上方文本框内填入或选择MAA更新器临时文件存储位置，默认为MAA根目录下MaaUpdater文件夹。\n";
+                FileStream fileStream = File.Create(".\\MaaUpdater.json");
+                fileStream.Close();
+            }
+        }
+        private void MaaPathChoose_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog()
+            {
+                InitialDirectory = Environment.CurrentDirectory,
+                Multiselect = false
+            };
+            if (openFolderDialog.ShowDialog() == true)
+            {
+                MaaPath.Text = openFolderDialog.FolderName;
+            }
+
+        }
+    }
+    public class ConfigJson
+    {
+        public ConfigJson(string filePath, string currentVersion, string latestVersion)
+        {
+            this.filePath = filePath;
+            this.currentVersion = currentVersion;
+            this.latestVersion = latestVersion;
+        }
+        public string filePath;
+        public string currentVersion;
+        public string latestVersion;
+    }
+}
